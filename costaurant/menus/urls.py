a30bc70ae710e4fa4e0222bdfa47b9f9a6488a06
OSHMOS <ojames9707@gmail.com>
2022-02-18 18:45:46 +0900

    금요일 개인 공부 : 코드잇 - 장고 ProJ 코스토랑

--- costaurant/menus/urls.py
+++ costaurant/menus/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('menus/', views.index)
+    path('', views.index),
+    path('<str:food>/', views.food_detail),  # 우아한 URL(장고의 동적 URL)
 ]
--- costaurant/menus/views.py
+++ costaurant/menus/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'menus/index.html')
+    today = datetime.today().date()
+    # print(today) # 밑의 검은 화면(콘솔창)에 출력됨
+    ctx = {'today': today}
+    return render(request, 'menus/index.html', ctx)
+
+
+# def chicken(request):
+#     return render(request, 'menus/chicken.html')
+
+
+def food_detail(request, food):
+    ctx = {'name': food}
+    return render(request, 'menus/detail.html', ctx)
