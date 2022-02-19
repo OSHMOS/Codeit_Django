0aea62ac7f3f4c5efd0d71808fb5457f43102e0b
OSHMOS <ojames9707@gmail.com>
2022-02-19 23:02:11 +0900

    토요일 개인 공부 : 코스토랑 템플릿, 뷰 이해하기

--- costaurant/menus/views.py
+++ costaurant/menus/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from datetime import datetime
+from django.http import Http404  # 의도하지 않은 404에러 (장고에 들어있는 기능) 가져오기
 
 # Create your views here.
 
@@ -16,5 +17,12 @@ def index(request):
 
 
 def food_detail(request, food):
-    ctx = {'name': food}
+    ctx = dict()
+    if food == "chicken":
+        ctx["name"] = "코딩에 빠진 닭"
+        ctx["description"] = "주머니가 가벼운 당신의 마음까지 생각한 가격 !"
+        ctx["price"] = 10000
+        ctx["img_path"] = "menus/images/chicken.jpg"
+    else:
+        raise Http404("당신이 찾고 싶은 음식은 우리 메뉴에 없어요!")
     return render(request, 'menus/detail.html', ctx)
