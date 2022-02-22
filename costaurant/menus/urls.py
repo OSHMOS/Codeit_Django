f658edc0615d42bc26928add93180d6aaaa3033d
OSHMOS <ojames9707@gmail.com>
2022-02-22 10:10:44 +0900

    화요일 개인 공부 : 장고 ProJ 코스토랑 완성하기

--- costaurant/menus/urls.py
+++ costaurant/menus/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index),
-    path('<str:food>/', views.food_detail),  # 우아한 URL(장고의 동적 URL)
+    path('<int:pk>/', views.food_detail),  # 우아한 URL(장고의 동적 URL)
 ]
--- costaurant/menus/views.py
+++ costaurant/menus/views.py
@@ -1,28 +1,23 @@
 from django.shortcuts import render
 from datetime import datetime
 from django.http import Http404  # 의도하지 않은 404에러 (장고에 들어있는 기능) 가져오기
+from menus.models import Menu
 
 # Create your views here.
 
 
 def index(request):
+    ctx = dict()
     today = datetime.today().date()
     # print(today) # 밑의 검은 화면(콘솔창)에 출력됨
-    ctx = {'today': today}
+    menus = Menu.objects.all()
+    ctx["today"] = today
+    ctx["menus"] = menus
     return render(request, 'menus/index.html', ctx)
 
 
-# def chicken(request):
-#     return render(request, 'menus/chicken.html')
-
-
-def food_detail(request, food):
+def food_detail(request, pk):
     ctx = dict()
-    if food == "chicken":
-        ctx["name"] = "코딩에 빠진 닭"
-        ctx["description"] = "주머니가 가벼운 당신의 마음까지 생각한 가격 !"
-        ctx["price"] = 10000
-        ctx["img_path"] = "menus/images/chicken.jpg"
-    else:
-        raise Http404("당신이 찾고 싶은 음식은 우리 메뉴에 없어요!")
+    menu = Menu.objects.get(id=pk)
+    ctx["menu"] = menu
     return render(request, 'menus/detail.html', ctx)
